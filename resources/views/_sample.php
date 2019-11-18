3d20829770fb4ee5ad98b824adcf80e0871d0367
Darana Vidya <darana.sv@gmail.com>
2019-11-18 21:44:05 +0700

    default blade

---
+++ resources/views/_sample.php
@@ -0,0 +1,14 @@
+@extends('template')
+@section('title', 'PROJECT NAME')
+
+@section('style')
+<!-- script -->
+@endsection
+
+@section('content')
+	<h1>Welcome</h1>
+@endsection
+
+@section('script')
+<!-- script -->
+@endsection
